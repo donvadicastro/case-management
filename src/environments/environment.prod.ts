d26adecdb330462c241af1d9dbf8d2115cb09d61
Vadym <vadim.barylo@intapp.com>
2021-11-12 09:40:46 +0200

    change config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBW2VlfzUrxKhMeyHU3H0MZJ1SGijs6pq4",
+    authDomain: "casemanagement-d7eb6.firebaseapp.com",
+    projectId: "casemanagement-d7eb6",
+    storageBucket: "casemanagement-d7eb6.appspot.com",
+    messagingSenderId: "432654386248",
+    appId: "1:432654386248:web:40c329bd54c52ca000c47d"
+  },
   production: true
 };
