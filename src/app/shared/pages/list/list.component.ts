36d52e92c1318a75bc7d43a1753cd7195e9a6f75
Vadym <vadim.barylo@intapp.com>
2021-11-12 10:24:50 +0200

    add loading

--- src/app/shared/pages/list/list.component.ts
+++ src/app/shared/pages/list/list.component.ts
@@ -1,12 +1,17 @@
 import {AngularFirestore} from "@angular/fire/compat/firestore";
 import {Observable} from "rxjs";
 import {BaseModel} from "../../entities/baseModel";
+import {tap} from "rxjs/operators";
 
 export abstract class AbstractListComponent<T extends BaseModel> {
+  public loading = false;
   public entities: Observable<T[]>;
 
   protected constructor(private collectionName: string, private store: AngularFirestore) {
+    this.loading = true;
+
     this.entities = this.store.collection(this.collectionName).valueChanges({idField: 'id'}) as Observable<T[]>
+    this.entities.pipe(tap(() => this.loading = false)).subscribe();
   }
 
   public onDelete(id: string) {
