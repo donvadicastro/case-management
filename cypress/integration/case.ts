4536439b3e37b9b4f22de9ff9f0a385b104e4d60
Vadym <vadim.barylo@intapp.com>
2021-11-21 20:57:36 +0200

    more cases

--- cypress/integration/case.ts
+++ cypress/integration/case.ts
@@ -46,7 +46,23 @@ describe('UseCase Test', () => {
     cy.get('input[formcontrolname=description]').type(id + 'Entity Description');
     cy.get('.btn-primary').should('contain', 'Submit').should('not.be.disabled').click();
 
-    // create use-case
+    // create query
+    cy.get('.nav-link').contains('QUERIES').click();
+    cy.get('.btn-primary').should('contain', 'Add Query').click();
+    cy.get('input[formcontrolname=name]').type(id + 'Query');
+    cy.get('select[formcontrolname=from]').select(id + 'Entity');
+    cy.get('input[formcontrolname=where]').type(id + 'Query Where');
+    cy.get('input[formcontrolname=description]').type(id + 'Query Description');
+    cy.get('.btn-primary').should('contain', 'Submit').should('not.be.disabled').click();
+
+    // create function
+    cy.get('.nav-link').contains('FUNCTIONS').click();
+    cy.get('.btn-primary').should('contain', 'Add Function').click();
+    cy.get('input[formcontrolname=name]').type(id + 'Function');
+    cy.get('input[formcontrolname=description]').type(id + 'Function Description');
+    cy.get('.btn-primary').should('contain', 'Submit').should('not.be.disabled').click();
+
+    // create use-case with entity and check
     cy.get('.nav-link').contains('CASES').click();
     cy.get('.btn-primary').should('contain', 'Add Case').click();
     cy.get('select[formcontrolname=actor]').select(id + 'Actor');
@@ -54,8 +70,24 @@ describe('UseCase Test', () => {
     cy.get('#typeEntity').check();
     cy.get('select[formcontrolname=entity]').select(id + 'Entity');
     cy.get('.btn-primary').should('contain', 'Submit').should('not.be.disabled').click();
-
-    // check
     cy.get('table').find('td').should('contain', `${id}Actor ${id}Action ${id}Entity`)
+
+    // create use-case with query and check
+    cy.get('.btn-primary').should('contain', 'Add Case').click();
+    cy.get('select[formcontrolname=actor]').select(id + 'Actor');
+    cy.get('select[formcontrolname=action]').select(id + 'Action');
+    cy.get('#typeQuery').check();
+    cy.get('select[formcontrolname=query]').select(id + 'Query');
+    cy.get('.btn-primary').should('contain', 'Submit').should('not.be.disabled').click();
+    cy.get('table').find('td').should('contain', `${id}Actor ${id}Action ${id}Query`)
+
+    // create use-case with query and check
+    cy.get('.btn-primary').should('contain', 'Add Case').click();
+    cy.get('select[formcontrolname=actor]').select(id + 'Actor');
+    cy.get('select[formcontrolname=action]').select(id + 'Action');
+    cy.get('#typeFunction').check();
+    cy.get('select[formcontrolname=function]').select(id + 'Function');
+    cy.get('.btn-primary').should('contain', 'Submit').should('not.be.disabled').click();
+    cy.get('table').find('td').should('contain', `${id}Actor ${id}Action ${id}Function`)
   })
 })
